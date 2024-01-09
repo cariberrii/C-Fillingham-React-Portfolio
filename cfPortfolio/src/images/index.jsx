66623eaf3cb41e731c5c79e63877fb36e862ee98
cariberrii <ca.fillingham@gmail.com>
2024-01-09 20:28:43 +0000

    Corrects path to deployment

---
+++ cfPortfolio/src/images/index.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import image1 from "./profile_pic.jpg";
+import image2 from "./images/image2.jpg";
+import image3 from "./images/image3.jpg";
+
+const imagesList = [
+    {
+      id: 1,
+      src: image1,
+      alt: "Image 1",
+    },
+    {
+      id: 2,
+      src: image2,
+      alt: "Image 2",
+    },
+    {
+      id: 3,
+      src: image3,
+      alt: "Image 3",
+    },
+  ];
--- cfPortfolio/vite.config.js
+++ cfPortfolio/vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/C-Fillingham-React-Portfolio/",
   plugins: [react()],
 })
